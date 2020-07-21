c0841e0469919e50b77af856dbd70320eba9569f
Barys_Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2020-07-20 17:47:47 -0700

    Added class Settings()

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """Class to store all game Settings"""
+
+    def __init__(self):
+        """screen settings"""
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
