a9873afe2d373c78228a3f51d6517a72e04c3aa6
Barys_Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2020-07-21 10:57:18 -0700

    Added  bullets

--- game_fynctions.py
+++ game_fynctions.py
@@ -1,16 +1,21 @@
 import sys
 import pygame
+from bullet import Bullet
 
 
-def check_keydown_events(event,ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """KeyDOWN event"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        # Create new bullet and adding it to the Group bullets.
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
-def check_keyup_events(event,ship):
+def check_keyup_events(event, ship):
     """KeyUP event"""
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
@@ -18,31 +23,25 @@ def check_keyup_events(event,ship):
         ship.moving_left = False
 
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     """Handles a key press and a mouse event"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event,ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-
-
-
-
-
-
-
-
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """Drawing BackGround screen color"""
     """Drawing every time during  each loop"""
     screen.fill(ai_settings.bg_color)
+    # All bullets shown behind Ship and Aliences
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
 
     """Tracking the last drawn screen """
     pygame.display.flip()
-
