e47908b3e4d6931459c60d09ddddcdc2dc7efe9e
Barys_Tsibets <65245525+BarysTsibets@users.noreply.github.com>
2020-07-20 18:32:24 -0700

    Added class Ship(), added ship and alien image

---
+++ ship.py
@@ -0,0 +1,22 @@
+import pygame
+
+class Ship:
+
+    def __init__(self, screen):
+        """Initialising ship and set ship starting position"""
+        self.screen = screen
+
+        """Loading image and get rectangle"""
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        """New ship will be centered in the  bottom of the screen """
+        self.rect.centerex = self.screen_rect.centerex
+        self.rect.bottom = self.screen_rect.bottom
+
+    def blitme(self):
+        """Drawing ship in the current position"""
+        self.screen.blit(self.image, self.rect)
+
+
